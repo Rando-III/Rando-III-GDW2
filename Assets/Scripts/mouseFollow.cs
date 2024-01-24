188985a0604a17c6fb29c02015f97525afb62273
Shallowcat <joshR364@hotmail.com>
2024-01-24 14:22:38 -0500

    stuff

--- Assets/Scripts/mouseFollow.cs
+++ Assets/Scripts/mouseFollow.cs
@@ -9,6 +9,12 @@ public class mouseFollow : MonoBehaviour
     [SerializeField] Camera mainCam;
 
     [SerializeField] Transform _transform;
+    [SerializeField] Renderer _renderer;
+
+    int green;
+    int blue;
+    int red;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,7 @@ public class mouseFollow : MonoBehaviour
     void Update()
     {
         
+        _renderer.material.color = new Color (red * 130, green * 130, blue * 130);
     }
 
     private void OnMouse(InputValue value)
@@ -30,5 +37,44 @@ public class mouseFollow : MonoBehaviour
         _transform.position = new Vector3(mouse.x, mouse.y);
         
 
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            if (collision.tag == "Blue")
+            {
+                blue++;
+            }
+            else if (collision.tag == "Red")
+            {
+                red++;
+            }
+            else if (collision.tag == "Green")
+            {
+                green++;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            if (collision.tag == "Blue")
+            {
+                blue--;
+            }
+            else if (collision.tag == "Red")
+            {
+                red--;
+            }
+            else if (collision.tag == "Green")
+            {
+                green--;
+            }
+        }
     }
 }
