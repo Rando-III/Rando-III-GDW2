7b73390b23d1f7641d96b7ec017381ee1f69267e
Shallowcat <joshR364@hotmail.com>
2024-01-24 13:51:29 -0500

    Spotlight basic

--- Assets/Scripts/mouseFollow.cs
+++ Assets/Scripts/mouseFollow.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class mouseFollow : MonoBehaviour
 {
+
+    [SerializeField] Camera mainCam;
+
+    [SerializeField] Transform _transform;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,15 @@ public class mouseFollow : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnMouse(InputValue value)
+    {
+        var mouse_pos = value.Get<Vector2>();
+        var mouse = mainCam.ScreenToWorldPoint(mouse_pos);
+        
+        _transform.position = new Vector3(mouse.x, mouse.y);
+        
+
     }
 }
