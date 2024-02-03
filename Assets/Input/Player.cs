c586155264a93198915e62abb64ffff066f63202
Alex-Hamblin <alexmachamblin@gmail.com>
2024-02-03 14:53:57 -0500

    Changed how movement is implemented for a smoother experience

--- Assets/Input/Player.cs
+++ Assets/Input/Player.cs
@@ -9,31 +9,57 @@ public class Player : MonoBehaviour
     public Vector2 _moveDirection;
     public float speed;
     public bool isGrounded = true;
-   
+    public bool canMove = true;
+    public Rigidbody2D rb;
+    public float maxVelocityX = 10;
+    public float minVelocityX = -10;
+    public bool isjumping = false;
+    
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
         InputManager.Init(this);
         InputManager.SetGameControls();
+        rb = GetComponent<Rigidbody2D>();
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += (Vector3)(speed * Time.deltaTime * _moveDirection);
-          
-        
+
+        rb.AddForce((new Vector2(10,0) * _moveDirection));
+        if (rb.velocity.x >= 10)
+        {
+            rb.velocity -= new Vector2( 1,0);
+        }
+        if (rb.velocity.x <= -10)
+        {
+            rb.velocity += new Vector2(1, 0);
+        }
+
+
+
     }
     public void SetMovementDirection(Vector2 currentDirection)
     {
+        
         _moveDirection = currentDirection;
+        
+        
+        
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 3)
         {
             isGrounded = true;
+            canMove = true;
+            isjumping = false;
         }
     }
     
@@ -42,6 +68,8 @@ public class Player : MonoBehaviour
         if (collision.gameObject.layer == 3)
         {
             isGrounded = false;
+            canMove = false;
+            
         }
     }
 
@@ -49,6 +77,7 @@ public class Player : MonoBehaviour
     {
         if (isGrounded) 
         {
+             isjumping = true;
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 10f), ForceMode2D.Impulse);
         }
         
