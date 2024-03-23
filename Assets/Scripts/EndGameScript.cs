3ac489438c7c8d1677e53cab414f859a5b7ac210
Alex-Hamblin <alexmachamblin@gmail.com>
2024-03-23 14:55:23 -0400

    teleporter level 1

---
+++ Assets/Scripts/EndGameScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameScript : MonoBehaviour
+{
+    public bool countdown;
+    public float time;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown)
+        {
+            time -= Time.deltaTime;
+        }
+        if (time <= 0)
+        {
+            End();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartEnd();
+        }
+    }
+    void StartEnd()
+    {
+        player.SetActive(false);
+        countdown = true;
+    }
+    void End()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+}
