0236644215d0719d200b50db71c2eb0070a79880
Alex-Hamblin <alexmachamblin@gmail.com>
2024-04-03 22:46:05 -0400

    Update EndGameScript.cs

--- Assets/Scripts/EndGameScript.cs
+++ Assets/Scripts/EndGameScript.cs
@@ -15,7 +15,7 @@ public class EndGameScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        fade.SetActive(true);
     }
 
     // Update is called once per frame
